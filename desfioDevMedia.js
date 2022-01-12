746e5772df547ba2d21fa8a0d50d6558a95e4eb0
anderoidy <anderoidy@gmail.com>
2022-01-12 03:10:44 -0300

    desafio finalizado

--- desfioDevMedia.js
+++ desfioDevMedia.js
@@ -1,21 +1,41 @@
 //Desafio de estagiario da DevMedia "Minha solucao"
+function entradaDosDados() {
+    var readlineSync = require('readline-sync');
 
-var readlineSync = require('readline-sync');
-
-while (numUser != 1234) {
+    while (numUser != 1234) {
     var numUser = readlineSync.question('Entre com o numero do usuario! ');
     if(numUser != 1234) {
         console.log("User incorreto ");
-    }  
-} 
+        }  
+    } 
 
-var passUser = readlineSync.question('Entre com o password! ');
+    var passUser = readlineSync.question('Entre com o password! ');
 if(passUser == 9999) {
     console.log("Acesso permitido") 
     console.log("\nO numero do usuario é: ", numUser);
     console.log("O password é: ", passUser);
 } else {
-    console.log("Acesso nao permitido ");
+    console.log("Acesso nao permitido ");    
+    }
+
+    var opcao = readlineSync.question('Tecle 0 para continuar ou 1 para sair! ');
+
+    switch(opcao) {
+
+        case "0":
+            entradaDosDados();        
+        break;        
+        case "1":
+        console.log("Até a proxima ");
+        break;
+        default: 
+            while(opcao != "0" && opcao != "1") {
+                console.log("Opção invalida");                
+                var opcao = readlineSync.question('Tecle 0 para continuar ou 1 para sair! ');
+            }
+            entradaDosDados();                    
+        }
 }
 
+entradaDosDados();
 

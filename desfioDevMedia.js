df01cdff5a5942aa2b68bed7bd2b7eef855a1ec3
anderoidy <anderoidy@gmail.com>
2022-01-12 03:29:23 -0300

    corregido variavel declarado no inicio e nao no meio do codigo

--- desfioDevMedia.js
+++ desfioDevMedia.js
@@ -1,16 +1,18 @@
 //Desafio de estagiario da DevMedia "Minha solucao"
 function entradaDosDados() {
     var readlineSync = require('readline-sync');
+    var userValido = 1234;
+    var senhaValida = 9999;
 
-    while (numUser != 1234) {
-    var numUser = readlineSync.question('Entre com o numero do usuario! ');
+    while (numUser != userValido) {
+    var numUser = readlineSync.question('\nEntre com o numero do usuario! ');
     if(numUser != 1234) {
         console.log("User incorreto ");
         }  
     } 
 
     var passUser = readlineSync.question('Entre com o password! ');
-if(passUser == 9999) {
+if(passUser == senhaValida) {
     console.log("Acesso permitido") 
     console.log("\nO numero do usuario é: ", numUser);
     console.log("O password é: ", passUser);
@@ -18,7 +20,7 @@ if(passUser == 9999) {
     console.log("Acesso nao permitido ");    
     }
 
-    var opcao = readlineSync.question('Tecle 0 para continuar ou 1 para sair! ');
+    var opcao = readlineSync.question('\nTecle 0 para continuar ou 1 para sair! ');
 
     switch(opcao) {
 
@@ -31,7 +33,7 @@ if(passUser == 9999) {
         default: 
             while(opcao != "0" && opcao != "1") {
                 console.log("Opção invalida");                
-                var opcao = readlineSync.question('Tecle 0 para continuar ou 1 para sair! ');
+                var opcao = readlineSync.question('Tecle 0 para continuar ou 1 para sair! \n');
             }
             entradaDosDados();                    
         }
